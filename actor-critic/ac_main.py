2da2a017455210524055a6bd436d3f88c61959d0
leonjovanovic <leonjovanovic1996@gmail.com>
2021-05-14 22:35:04 +0200

    Tunning hyperparameters

--- actor-critic/ac_main.py
+++ actor-critic/ac_main.py
@@ -1,47 +1,53 @@
 
 import gym
+import numpy as np
+
 from agent import Agent
 from torch.utils.tensorboard import SummaryWriter
 import time
 # -----------------------PARAMETERS---------------------------------
 HYPERPARAMETERS = {
-    'learning_rate_actor': 0.0005,
-    'learning_rate_critic': 0.0007,
+    'learning_rate_actor': 0.0007,
+    'learning_rate_critic': 0.0008,
     'gamma': 0.99,
-    'random_seed': -1,
+    'random_seed': 12,
     'entropy': True,
-    'entropy_beta': 0.1,
-    'n-step': 3
+    'entropy_beta': 0.001,
+    'n-step': 2
 }
 ENV_NAME = 'CartPole-v1'
 WRITER = True
-LOG_DIR = 'content/runs/ActorCritic'
-MAX_EPISODES = 4000
+LOG_DIR = 'content/runs/ActorCritic16163232781200012'
+MAX_EPISODES = 1000
 #--------------------------------------------------------------------
 env = gym.make(ENV_NAME)
 obs = env.reset()
-writer = SummaryWriter(log_dir=LOG_DIR + str(time.time()))
-agent = Agent(env=env, hyperparameters=HYPERPARAMETERS, writer=writer)
-ep_num = 0
-start = time.time()
-while ep_num < MAX_EPISODES:
-    #env.render()
-    action = agent.choose_action(obs)
-    new_obs, reward, done, _ = env.step(action)
-    if done:
-        reward = -20
-    agent.improve_params(obs, action, new_obs, reward, done)
-    obs = new_obs
-    if done:
-        obs = env.reset()
-        avg_reward = agent.print_state(ep_num)
-        ep_num += 1
-        if avg_reward >= 495:
-            stop = time.time()
-            print(stop-start)
-            break
+avg_ep = []
+for _ in range(10):
+    writer = SummaryWriter(log_dir=LOG_DIR + str(time.time()))
+    agent = Agent(env=env, hyperparameters=HYPERPARAMETERS, writer=writer)
+    ep_num = 0
+    start = time.time()
+    while ep_num < MAX_EPISODES:
+        #env.render()
+        action = agent.choose_action(obs)
+        new_obs, reward, done, _ = env.step(action)
+        if done:
+            reward = -20
+        agent.improve_params(obs, action, new_obs, reward, done)
+        obs = new_obs
+        if done:
+            obs = env.reset()
+            avg_reward = agent.print_state(ep_num)
+            ep_num += 1
+            if avg_reward >= 495:
+                stop = time.time()
+                print("Episodes " + str(ep_num) + " " + str(stop-start))
+                avg_ep.append(ep_num)
+                break
+    writer.close()
+    print(np.mean(avg_ep))
 env.close()
-writer.close()
 # !tensorboard --logdir "D:\Users\Leon Jovanovic\Documents\Computer Science\Reinforcement Learning\deep-reinforcement-learning-pg-cartpole\actor-critic\content\runs" --host=127.0.0.1
 
 #1450 seconds
