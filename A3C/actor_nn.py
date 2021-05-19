1885f4c8ab3d9b22a01a8e8067996b7772bfcf6e
leonjovanovic <leonjovanovic1996@gmail.com>
2021-05-20 01:05:12 +0200

    A3C agent control

---
+++ A3C/actor_nn.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class ActorNN(nn.Module):
+    def __init__(self, input_shape, output_shape, seed):
+        super(ActorNN, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_shape, 16),
+            nn.ReLU(),
+            nn.Linear(16, 16),
+            nn.ReLU(),
+            nn.Linear(16, output_shape),
+            nn.Softmax(dim=-1)
+        )
+        self.model.double()
+
+    def forward(self, x):
+        return self.model(x)
+
--- A3C/process.py
+++ A3C/process.py
@@ -7,8 +7,10 @@ HYPERPARAMETERS = {
     'lr_actor': 0.001,
     'lr_critic': 0.001,
     'gamma': 0.9,
-    'entropy_flag': True,
-    'entropy': 0.001,
+    'n-step': 2,
+    'entropy_flag': False,
+    'entropy_coef': 0.001,
+    'seed': 12
 }
 #--------------------------------------------------------------
 env = gym.make(ENVIROMENT_NAME)
@@ -19,10 +21,11 @@ for ep_num in range(MAX_WORKER_GAMES):
     new_obs, reward, done, _ = env.step(action)
     if done:
         reward = -20
-    agent.improve(obs, reward, new_obs, done)
+    agent.improve(obs, reward, new_obs, action)
     obs = new_obs
     if done:
-        agent.reset()
+        agent.reset(ep_num)
         ep_num += 1
+        break
         #Proveriti kada ga gasimo
 
